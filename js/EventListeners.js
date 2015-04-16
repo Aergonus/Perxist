38c79c8a4c768b983a7bc0a3bfbb2c941bc21a85
Spellchaser <KangqiaoLei@gmail.com>
2015-04-16 15:17:45 -0400

    Revert Scramble

--- js/EventListeners.js
+++ js/EventListeners.js
@@ -26,3 +26,25 @@ function onDocumentKeyDown(event)
 	console.log("Keydown Listener");
 	toRot(event.keyCode, event.shiftKey);
 }
+
+var rotations = [];
+function ScrambleCube()
+{
+	console.log( "Scrambling!" );
+	var nRot = document.getElementById("ScrambleNumber");
+	console.log( nRot );
+	var rotKeys = Object.keys(keyMappings);
+	console.log( rotKeys );
+	var rKC = rotKeys[Math.floor(Math.random()*rotKeys.length)];
+	console.log( "Init rKC: " + rKC);
+	for (var i = 0; i < nRot; ++i) 
+	{
+		console.log( "Loop #" + i );
+		rKC = rotKeys[Math.floor(Math.random()*rotKeys.length)];
+		console.log( "Current rKC:" + rKC );
+		// TODO: Random directions
+		toRot(rKC, direction);
+		rotations.push(function() {toRot(rKC, direction)});
+		console.log( "Rotated!");
+	}
+}
