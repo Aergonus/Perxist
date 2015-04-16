66a30487264022db21370d325c8fc71914e5b3bc
Spellchaser <victorkanglei@yahoo.com>
2015-04-16 15:08:29 -0400

    Scramble

--- js/ScrambleCube.js
+++ js/ScrambleCube.js
@@ -2,24 +2,26 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+var rotations = [];
+
 function ScrambleCube()
 {
-	console.log( "Scrambling!" ):
+	console.log( "Scrambling!" );
 	var nRot = document.getElementById("ScrambleNumber");
-	console.log( nRot ):
+	console.log( nRot );
 	var rotKeys = Object.keys(keyMappings);
-	console.log( rotKeys ):
+	console.log( rotKeys );
 	var rKC = rotKeys[Math.floor(Math.random()*rotKeys.length)];
-	console.log( "Init rKC: " + rKC):
+	console.log( "Init rKC: " + rKC);
 	for (var i = 0; i < nRot; ++i) 
 	{
-		console.log( "Loop #" + i ):
+		console.log( "Loop #" + i );
 		rKC = rotKeys[Math.floor(Math.random()*rotKeys.length)];
-		console.log( "Current rKC:" + rKC ):
+		console.log( "Current rKC:" + rKC );
 		// TODO: Random directions
-		direction = event.shiftKey ? 1 : -1;
 		toRot(rKC, direction);
-		console.log( "Rotated!"):
+		rotations.push(function() {toRot(rKC, direction)});
+		console.log( "Rotated!");
 	}
 }
 
