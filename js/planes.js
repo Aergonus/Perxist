9f8c4a0bb2dd354426e857f41d3f7f3bb731b933
Spellchaser <KangqiaoLei@gmail.com>
2015-05-11 10:36:21 -0400

    Sides!

--- js/planes.js
+++ js/planes.js
@@ -23,7 +23,20 @@ function createPlanes()
 	material = new THREE.MeshLambertMaterial({map: texture, fog: true});
 
 	plane = new THREE.Mesh( geometry, material );
-	plane.position.set(-20, -100, 0);
+	plane.position.set(-10, 100, 0);
+	planeArray.push(plane);
+
+	geometry = new THREE.PlaneGeometry( 200, 400, 10, 20 );
+	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2) ); // Makes plane "horizontal" instead of "vertical"
+	geometry.applyMatrix( new THREE.Matrix4().makeRotationZ(   Math.PI / 2) );
+	var texture = THREE.ImageUtils.loadTexture('media/ground.png');
+	texture.wrapS = THREE.RepeatWrapping;
+	texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set( 8, 16 );
+	material = new THREE.MeshLambertMaterial({map: texture, fog: true});
+
+	plane = new THREE.Mesh( geometry, material );
+	plane.position.set(10, 100, 0);
 	planeArray.push(plane);
 	
 	return planeArray;
