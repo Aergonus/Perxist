4aedeb3acd3d015a3272c5790068ed848c21f495
Spellchaser <KangqiaoLei@gmail.com>
2015-05-11 09:52:42 -0400

    Check all textures work

--- js/cubes.js
+++ js/cubes.js
@@ -49,26 +49,26 @@ function setCubeMaterial()
 {
 	var cubeMaterial = [];
 	var curTexture;
-	var randText = Math.floor(4 * Math.random());
+	var randText = Math.floor(8 * Math.random());
 	switch(randText) {
 		case 0:
-		//	curTexture = texture1; break;
+			curTexture = texture1; break;
 		case 1:
 			curTexture = texture2; break;
 		case 2:
-		//	curTexture = texture3; break;
+			curTexture = texture3; break;
 		case 3:
 			curTexture = texture4; break;
 		case 4:
-		//	curTexture = texture5; break;
+			curTexture = texture5; break;
 		case 5:
-		//	curTexture = texture6; break;
+			curTexture = texture6; break;
 		case 6:
-		//	curTexture = texture7; break;
+			curTexture = texture7; break;
 		case 7:
-		//	curTexture = texture8; break;
+			curTexture = texture8; break;
 		case 8:
-		//	curTexture = texture9; break;
+			curTexture = texture9; break;
 	}
 	
 	for (var i = 0; i < 6; ++i)
