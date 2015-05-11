32125fe9038262e839cfd112bac94998d09f0691
Spellchaser <KangqiaoLei@gmail.com>
2015-05-11 11:37:33 -0400

    Faster movement., less control

--- js/game.js
+++ js/game.js
@@ -228,11 +228,11 @@ function updateWorld(delta)
 	var isOnObject = intersections.length > 0;
 
 	velocity.x -= velocity.x * 10.0 * delta;
-	velocity.z -= velocity.z * 10.0 * delta + 5;
+	velocity.z -= velocity.z * 10.0 * delta + 9.8;
 
 	velocity.y -= 7.8 * 10.0 * delta; // 100.0 = mass
 
-	if ( moveForward ) velocity.z -= 400.0 * delta;
+	if ( moveForward ) velocity.z -= 20.0 * delta;
 	if ( moveBackward ) velocity.z += 4.0 * delta;
 
 	if ( moveLeft ) velocity.x -= 400.0 * delta;
