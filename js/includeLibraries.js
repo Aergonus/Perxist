5678c79f914357ed7e060702426f76965d3085f8
Spellchaser <KangqiaoLei@gmail.com>
2015-05-10 21:55:10 -0400

    Box with Ground Plane

---
+++ js/includeLibraries.js
@@ -0,0 +1,7 @@
+			<script type="text/javascript" src="./js/Libraries/three.min.js"></script>
+			<script type="text/javascript" src="./js/Libraries/stats.min.js"></script>
+			<script type="text/javascript" src="./js/Libraries/Detector.js"></script>
+			<script type="text/javascript" src="./js/Libraries/MouseControls.js"></script>
+			<script type="text/javascript" src="./js/Libraries/FlyControls.js"></script>
+			<script type="text/javascript" src="./js/Libraries/FirstPersonControls.js"></script>
+			<script type="text/javascript" src="./js/Libraries/PointerLockControls.js"></script>
