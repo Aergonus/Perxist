e792c87abb5dc9742f676398a6f57be064a56c42
Spellchaser <KangqiaoLei@gmail.com>
2015-05-13 12:25:59 -0400

    Detect collisions better

--- js/game.js
+++ js/game.js
@@ -221,7 +221,7 @@ function createLights()
 }
 
 var checkDownRay = new THREE.Vector3( 0, - 1, 0 );
-var raycaster = new THREE.Raycaster( new THREE.Vector3(), checkDownRay.clone(), 1.501, 1.8 );
+var raycaster = new THREE.Raycaster( new THREE.Vector3(), checkDownRay.clone(), 1.501, 200 );
 var raycasterDown = new THREE.Raycaster( new THREE.Vector3(), checkDownRay.clone(), 1.5);
 
 var rays = [
@@ -279,8 +279,8 @@ function updateWorld(delta)
 			console.log(velocity.y, (- velocity.y * delta), intersections[0].distance);
 		if (velocity.y < 0 && (- velocity.y * delta) > (intersections[0].distance + 1.5) )	{
 			cubie.position.y = intersections[0].object.position.y + 1.55;
-		}
 			velocity.y = Math.max( 0, velocity.y );
+		}
 			canJump = true;
 			updateY = false;
 	}
